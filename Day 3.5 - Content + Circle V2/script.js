e64c4811bd70815642b8ce3d329ca1b87eb89400
jmadarang87 <jmadarang87@gmail.com>
2021-03-19 00:23:43 -0500

    Content + Circle - Slide In + Out

---
+++ Day 3.5 - Content + Circle V2/script.js
@@ -0,0 +1,18 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+const circle = document.querySelector('.circle');
+const nav = document.querySelector('.nav');
+
+open.addEventListener('click', () => {
+    container.classList.add('show-nav');
+    circle.classList.add('show-nav');
+    nav.classList.add('show-nav');
+})
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+    circle.classList.remove('show-nav');
+    nav.classList.remove('show-nav');
+})
+

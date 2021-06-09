5f7606d97eb5e6d727d6eb055bc25c1ff432e159
jmadarang87 <jmadarang87@gmail.com>
2021-06-09 00:52:44 -0500

    updated calculator display

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,12 +1,37 @@
-console.log('calculator');
-const output = document.getElementById('output');
-const one = document.getElementById('1');
-const buttons = document.querySelectorAll('button');
+const output = document.getElementById('section-output');
+const oneBtn = document.getElementById('1');
+const twoBtn = document.getElementById('2');
+const threeBtn = document.getElementById('3');
+const fourBtn = document.getElementById('4');
+const fiveBtn = document.getElementById('5');
+const sixBtn = document.getElementById('6');
+const sevenBtn = document.getElementById('7');
+const eightBtn = document.getElementById('8');
+const nineBtn = document.getElementById('9');
+const zeroBtn = document.getElementById('0');
+const decimalBtn = document.getElementById('.');
+const clearBtn = document.getElementById('clear');
+const numBtns = document.getElementsByClassName('number');
+const operationBtns = document.querySelectorAll('operation');
+const display = document.getElementById('display');
+let preDisplay = "";
+let currentDisplay = "";
 
+const updateDisplay = (event) => {
+    if ( display.innerText === "0" ) {
+        let currentDisplay = "";
+        currentDisplay = event.target.id;
+        display.innerText = currentDisplay;
+    } else {
+        display.innerText += event.target.id;
+    }
+}
 
-buttons.forEach(button => {
-    button.addEventListener( 'click', (event) => {
-        let value = event.target.id;
-        output.innerHTML = value;
-    })
-});
+for (i = 0; i < numBtns.length; i++) {
+    numBtns[i].addEventListener('click', updateDisplay, false);
+};
+
+clearBtn.addEventListener( 'click', () => {
+    display.innerText = '';
+})
+
